7d2b6b09346180c8aa0198e8671bfea6d17899b9
Prashanth A <prashanth.gs2016@hotmail.com>
2020-07-05 21:08:52 +0530

    Added mvc to application

---
+++ Netcore-bookstore/Netcore-bookstore/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace Netcore_bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string index()
+        {
+            return "Shri ramakarunanandayanaha";
+        }
+    }
+}
